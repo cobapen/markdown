a3a59f3d3a5c307d5646d0fb8c5bf8fe9dd519f0
yamavol <yamavol@outlook.com>
2025-04-21 23:16:51 +0900

    Update api interfaces.
    Add more options and add custom method to install
    plugins to external markdown-it instance.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import markdownIt from "markdown-it";
+import markdownIt, { Options as MarkdownOptions } from "markdown-it";
 import { highlighterForMarkdownIt } from "./code/highlight.js";
 import anchor from "markdown-it-anchor";
 // @ts-ignore
@@ -9,66 +9,95 @@ import deflist from "markdown-it-deflist";
 import toc from "markdown-it-table-of-contents";
 // @ts-ignore
 import footnote from "markdown-it-footnote";
-import { MathjaxEngine } from "./math/mathjax.js";
+import { MathjaxEngine, Options as MathOptions } from "./math/mathjax.js";
 import { mdmath } from "./math/mdmath.js";
 import { fence_custom } from "./code/fence-custom.js";
 
-export interface Configuration {
+export interface Config {
   /**
    * Set "true" to display the title (if specified) of the fenced code block.
    * The title is hidden by default, and user must explicitly override the style.
    */
   showCodeTitleByDefault: boolean;
+
+  /**
+   * MarkdownIt options
+   */
+  markdown: Partial<MarkdownOptions>;
+
+  /**
+   * MathJax options
+   */
+  math: Partial<MathOptions>;
 }
 
-export type Options = Partial<Configuration>;
+export type Options = Partial<Config>;
 
-const defaultOptions: Configuration = {
+const defaultOptions: Config = {
   showCodeTitleByDefault: false,
+  markdown: {
+    html: true,
+    linkify: true,
+    highlight: highlighterForMarkdownIt,
+  },
+  math: {
+    tex: {
+      macros: {
+        bm: ["\\boldsymbol{#1}", 1],
+      },
+    },
+  }
 };
 
 export class MarkdownConverter {
-  private readonly _config: Configuration;
+  private readonly _config: Config;
   private readonly _mj: MathjaxEngine;
   private readonly _md: markdownIt;
 
   constructor(option?: Options) {
     const config = { ...defaultOptions, ...option };
-
-    const mj = new MathjaxEngine({
-      tex: {
-        macros: {
-          bm: ["\\boldsymbol{#1}", 1],
-        },
-      },
-    });
-    const md = markdownIt({
-      html: true,
-      linkify: true,
-      highlight: highlighterForMarkdownIt,
-    });
-
-    md.renderer.rules.fence = fence_custom;
-  
-    md.use(anchor)
-      .use(cjkbreaks)
-      .use(footnote)
-      .use(deflist)
-      .use(mdmath(mj))
-      .use(toc, {
-        includeLevel: [2, 3],
-      });
+    const mj = new MathjaxEngine(config.math);
+    const md = markdownIt(config.markdown);
 
     this._config = config;
     this._mj = mj;
     this._md = md;
+
+    this.setup(md);
   }
 
+  /**
+   * Install plugins and renderers to the markdown-it instance.
+   * 
+   * @param md The instance
+   */
+  public setup(md: markdownIt) {
+    md.renderer.rules.fence = fence_custom;
+    md.use(anchor)
+      .use(cjkbreaks)
+      .use(footnote)
+      .use(deflist)
+      .use(mdmath(this._mj))
+      .use(toc, {
+        includeLevel: [2, 3],
+      });
+  }
+
+  /**
+   * Render html from markdown.
+   * 
+   * @param text markdown text
+   * @returns html text
+   */
   public render(text: string): string {
     const env = {...this._config}; // env object must s
     return this._md.render(text, env);
   }
 
+  /**
+   * Returns the MathJax CSS.
+   * @returns 
+   */
   public mathcss(): string {
     return this._mj.stylesheet();
   }
