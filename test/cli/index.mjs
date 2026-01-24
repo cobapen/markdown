8cb35d5a1ab2c72739cd410b2c328a3a7c727257
yamavol <yamavol@outlook.com>
2026-01-24 17:30:13 +0900

    Replaced cli test with perf measure feature

--- test/cli/index.mjs
+++ test/cli/index.mjs
@@ -1,20 +1,44 @@
-import process from "process";
-import { CMarkdown } from "@cobapen/markdown";
+import { performance, PerformanceObserver } from "node:perf_hooks";
 
-const converter = new CMarkdown({
-  showCodeTitleByDefault: true,
-  markdown: {
-  },
-  math: {
+const obs = new PerformanceObserver(items => {
+  console.log("--- Performance Result ---");
+  for(const entry of items.getEntries()) {
+    console.log(`${entry.name}: ${entry.duration.toFixed(3)}ms`);
   }
 });
+obs.observe({ entryTypes: ["measure"], buffered: true });
 
-const inputText = process.argv[2];
-
-if (!inputText) {
-  console.error("Error: No input text.");
-  process.exit(1);
+function marker(name) {
+  return [name + "-start", name + "-end"];
 }
 
+const START = 0;
+const END = 1;
+const importMark = marker("import");
+const instMark = marker("inst");
+const convMark = marker("conv");
+
+performance.mark(importMark[START]);
+const { CMarkdown } = await import("../../lib/index.js");
+performance.mark(importMark[END]);
+
+performance.mark(instMark[START]);
+const converter = new CMarkdown({
+  showCodeTitleByDefault: true,
+  markdown: {},
+  math: {}
+});
+performance.mark(instMark[END]);
+
+const inputText = process.argv[2] || "No input";
+
+performance.mark(convMark[START]);
 const htmlOutput = converter.render(inputText);
+performance.mark(convMark[END]);
+
+performance.measure("Import Time\t", importMark[START], importMark[END]);
+performance.measure("Inst. Time\t", instMark[START], instMark[END]);
+performance.measure("Convert Time\t", convMark[START], convMark[END]);
+
 console.log(htmlOutput);
+
